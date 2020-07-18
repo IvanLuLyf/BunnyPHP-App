348a77585379e5703814d6081da9ab2bf3355d4c
IvanLuLyf <835498692@qq.com>
2020-07-18 20:16:27 +0800

    update index.php

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 header('X-Powered-By:BunnyFramework');
 define('APP_PATH', __DIR__ . '/');
 define('APP_DEBUG', true);
-define("IN_TWIMI_PHP", "True", TRUE);
 date_default_timezone_set('PRC');
 require 'vendor/autoload.php';
 (new BunnyPHP\BunnyPHP())->run();
