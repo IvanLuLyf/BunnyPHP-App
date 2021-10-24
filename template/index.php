675cc90ca8f0118cb7ae07260bd8a52558712e30
IvanLuLyf <835498692@qq.com>
2021-10-24 17:24:43 +0800

    update index

--- template/index.php
+++ template/index.php
@@ -9,6 +9,7 @@
             width: 35em;
             margin: 0 auto;
             text-align: center;
+            font-family: system-ui, -apple-system, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", "Liberation Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
         }
 
         .logo {
@@ -22,7 +23,7 @@
     <img src="/static/img/logo.png" class="logo" alt="BunnyPHP">
 </a>
 <h1><?= $_LANG['greeting'] ?><a href="https://github.com/IvanLuLyf/BunnyPHP">BunnyPHP</a>!</h1>
-<p><?= $_LANG['intro'] ?></p>
+<p><?= $_LANG['intro'] ?>🎉</p>
 <p><em><?= $_LANG['thank'] ?> Ver<?= BunnyPHP::BUNNY_VERSION ?></em></p>
 </body>
 </html>
